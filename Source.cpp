3ef08570902e52b3471674cbf1d6108c4ac4bc92
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2024-05-12 21:15:24 +0300

    added decipher

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,10 @@ string caesar_cipher(string plain_text, int key)
 	}
 	return plain_text;
 }
+string caesar_decipher(string ciphered_text, int key)
+{
+	return caesar_cipher(ciphered_text, 26-key);
+}
 void write_to_file(string file_name, string text)
 {
 	ofstream outfile(file_name+".txt");
@@ -27,16 +31,36 @@ void write_to_file(string file_name, string text)
 	outfile.close();
 }
 
-int main  ()
+int main()
 {
-	cout << "please enter plain text !"<<endl;
+	//choose ciphering or deciphering
+	cout << "press 1 for ciphering or 2 for deciphering\n" ;
+	int user_choice;
+	cin >> user_choice;
+	if (user_choice == 1)
+	{
+	
+	cout << "please enter plain text !\n" ;
+	cin.ignore();
 	string plain_text;
 	getline(cin, plain_text);
-	cout << "enter the key for caesar cipher : any number between 1 and 26"<<endl;
+	cout << "enter the key for caesar cipher : any number between 1 and 26" << endl;
 	int key;
 	cin >> key;
-	cout<<"the encrypted text is : "<<caesar_cipher(plain_text,key)<<endl;
-	write_to_file("ciphered text",caesar_cipher(plain_text, key));
-
+	cout << "the encrypted text is : " << caesar_cipher(plain_text, key) << endl;
+	write_to_file("ciphered text", caesar_cipher(plain_text, key));
+	}
+	else if (user_choice == 2)
+	{
+		cout << "please enter ciphered text !\n" ;
+		cin.ignore();
+		string ciphered_text;
+		getline(cin, ciphered_text);
+		cout << "enter the key for caesar decipher " << endl;
+		int key;
+		cin >> key;
+		cout << "the decrypted text is : " << caesar_decipher(ciphered_text, key) << endl;
+		write_to_file("plain text", caesar_decipher(ciphered_text, key));
+	}
 
 }
